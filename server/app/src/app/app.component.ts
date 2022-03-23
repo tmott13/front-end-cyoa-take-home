9e1098e6d0e74aca9cefa98648b14a8db05ca151
Tanya Mott <tanya.mott@na.crh.com>
2022-03-23 13:06:14 -0400

    added error handling

--- server/app/src/app/app.component.ts
+++ server/app/src/app/app.component.ts
@@ -7,12 +7,12 @@ import { IComment } from './models/comment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   commentForm = new FormGroup({
-    name: new FormControl(""),
-    comment: new FormControl("")
+    name: new FormControl(''),
+    comment: new FormControl(''),
   });
   commentList: IComment[] = [];
 
@@ -23,31 +23,46 @@ export class AppComponent {
   }
 
   getComments() {
-    this.appService.getComments().subscribe((comments) => {
+    this.appService.getComments().subscribe(
+      (comments) => {
         this.commentList = comments;
-        this.commentList.forEach(c => {
-          c.created = moment(c.created).format('LLLL');  
+        this.commentList.forEach((c) => {
+          c.created = moment(c.created).format('LLLL');
         });
-    });
+      },
+      (err) => {
+        console.log('Error caught in getComments() subscribe :' + err);
+      }
+    );
   }
 
   deleteComments() {
-    this.appService.deleteComments().subscribe(() => {
-      this.getComments();
-    });
+    this.appService.deleteComments().subscribe(
+      () => {
+        this.getComments();
+      },
+      (err) => {
+        console.log('Error caught in deleteComments() subscribe :' + err);
+      }
+    );
   }
 
   submit() {
     const request: IComment = {
-      name: this.commentForm.get("name").value,
+      name: this.commentForm.get('name').value,
       created: moment().format(),
-      message: this.commentForm.get("comment").value
+      message: this.commentForm.get('comment').value,
     };
     console.log(request);
-    this.appService.addComment(request).subscribe((x) => {
-      console.log(x);
-      this.commentForm.reset();
-      this.getComments();
-    });
+    this.appService.addComment(request).subscribe(
+      (x) => {
+        console.log(x);
+        this.commentForm.reset();
+        this.getComments();
+      },
+      (err) => {
+        console.log('Error caught in submit() subscribe :' + err);
+      }
+    );
   }
 }
